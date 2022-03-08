47935562e6c1043c1d24dbcbdaf3228af79ab121
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2022-03-08 14:31:45 +0100

    Add project files.

---
+++ Scala.Bestellingen.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Scala.Bestellingen.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void LstBestellingen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void BtnNieuweBestelling_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnWijzigBestelling_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnVerwijderBestelling_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnAnnuleerBestelling_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBewaarBestelling_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BestellijnBewerkingInschakelen()
+        {
+        }
+        private void BestellijnBewerkingUitschakelen()
+        {
+        }
+        private void BtnBestellijnNieuw_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBestellijnWijzig_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBestellijnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBewaarBestellijn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnAnnuleerBestellijn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
